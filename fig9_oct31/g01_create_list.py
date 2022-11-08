01487db188562d2d564965f553c3a4edc1b13bd5
jungheejung <>
2022-11-08 16:53:28 -0500

    BUG: create input city rows for the length of key destination cities, i.e. 151 times

--- fig9_oct31/g01_create_list.py
+++ fig9_oct31/g01_create_list.py
@@ -4,14 +4,15 @@ import numpy as np
 import os
 # load csv
 # %%
-fname = '/Users/h/Dropbox/projects_dropbox/other_sideprojects/green_climatechange/fig9_oct31/Large_airports_key.csv'
+key_fname = '/Users/h/Dropbox/projects_dropbox/other_sideprojects/green_climatechange/fig9_oct31/Large_airports_key.csv'
 fname = '/Users/h/Dropbox/projects_dropbox/other_sideprojects/green_climatechange/fig9_oct31/INPUT_ohbm_171819_freq.csv'
 save_dir = '/Users/h/Dropbox/projects_dropbox/other_sideprojects/green_climatechange/fig9_oct31/input_ohbm'
 airport = pd.read_csv(fname)
+keyairport = pd.read_csv(key_fname)
 
 # grab each row
 for index, row in airport.iterrows():
-    new_df = pd.DataFrame(columns = ['City', 'Country'], index = range(len(airport)))
+    new_df = pd.DataFrame(columns = ['City', 'Country'], index = range(len(keyairport)))
     # airport.iloc[index, 2] = str(row['location'].replace(" ", ""))
     # pd.concat(pd.Series(row[['City', 'Country']])*len(airport))
     new_df['City'] = row['City']
